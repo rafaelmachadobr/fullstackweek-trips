917fbc7069574e5252353674228953ee2777406a
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-07-16 15:55:07 -0300

    feat: add recommended trips component

---
+++ src/app/components/RecommendedTrips.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const RecommendedTrips = () => {
+  return (
+    <div className="container mx-auto p-5">
+      <div className="flex items-center">
+        <div className="w-full h-[1px] bg-grayLighter"></div>
+        <h2 className="px-5 font-medium text-grayPrimary whitespace-nowrap">
+          Destinos Recomendados
+        </h2>
+        <div className="w-full h-[1px] bg-grayLighter"></div>
+
+        <div className="flex flex-col items-center mt-5 gap-3">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedTrips;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import TripSearch from "./components/TripSearch";
 import QuickSearch from "./components/QuickSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   );
 }
