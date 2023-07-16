55183e8bcdfe8ef37b25c529f32e423294d3b737
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-07-16 11:26:19 -0300

    feat: add quick search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import TripSearch from "./components/TripSearch";
+import QuickSearch from "./components/QuickSearch";
 
 export default function Home() {
-  return <div>
-    <TripSearch />
-  </div>;
+  return (
+    <div>
+      <TripSearch />
+      <QuickSearch />
+    </div>
+  );
 }
