383f4e7d65b96e846b5e629539ceb86a746981a8
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-07-17 12:19:41 -0300

    style: add border to trip reservation component

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -36,7 +36,9 @@ const TripReservation = ({ trip }: TripReservationProps) => {
         <p className="font-medium text-sm text-primaryDarker">R$2500</p>
       </div>
 
-      <Button className="mt-3">Reservar agora</Button>
+      <div className="pb-10 border-b border-grayLighter w-full">
+      <Button className="mt-3 w-full">Reservar agora</Button>
+      </div>
     </div>
   );
 };
