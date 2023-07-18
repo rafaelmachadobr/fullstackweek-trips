b49f29217a13289216a2b0d57118291c864ce8c7
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-07-18 18:46:30 -0300

    feat: add user reservations page

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -3,10 +3,16 @@
 import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { TripReservation } from "@prisma/client";
+import { Prisma, TripReservation } from "@prisma/client";
+
+import UserReservationItem from "./components/UserReservationItem";
 
 const MyTrips = () => {
-  const [reservations, setReservations] = useState<TripReservation[]>([]);
+  const [reservations, setReservations] = useState<
+    Prisma.TripReservationGetPayload<{
+      include: { trip: true };
+    }>[]
+  >([]);
 
   const { status, data } = useSession();
 
@@ -18,9 +24,7 @@ const MyTrips = () => {
     }
 
     const fetchReservations = async () => {
-      const response = await fetch(
-        `http://localhost:3000/api/user/${(data?.user as any).id}/reservations`
-      );
+      const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any).id}/reservations`);
 
       const json = await response.json();
 
@@ -30,7 +34,14 @@ const MyTrips = () => {
     fetchReservations();
   }, [status]);
 
-  return <div>MyTrips</div>;
+  return (
+    <div className="container mx-auto p-5">
+      <h1 className="font-semibold text-primaryDarker text-xl">Minhas Viagens</h1>
+      {reservations?.map((reservation) => (
+        <UserReservationItem key={reservation.id} reservation={reservation} />
+      ))}
+    </div>
+  );
 };
 
 export default MyTrips;
