910fd16045549d260540fa678caa1e4c0efd2ebf
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-07-18 19:15:44 -0300

    feat: add reservation cancel

--- src/app/my-trips/components/UserReservationItem.tsx
+++ src/app/my-trips/components/UserReservationItem.tsx
@@ -5,6 +5,8 @@ import ReactCountryFlag from "react-country-flag";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Button from "@/components/Button";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
 interface UserReservationItemProps {
   reservation: Prisma.TripReservationGetPayload<{
@@ -13,7 +15,25 @@ interface UserReservationItemProps {
 }
 
 const UserReservationItem = ({ reservation }: UserReservationItemProps) => {
+  const router = useRouter();
+
   const { trip } = reservation;
+
+  const handleDeleteClick = async () => {
+    const res = await fetch(`/api/trips/reservation/${reservation.id}`, {
+      method: "DELETE",
+    });
+
+    if (!res.ok) {
+      return toast.error("Ocorreu um erro ao cancelar a reserva!");
+    }
+
+    toast.success("Reserva cancelada com sucesso!", {
+      position: "bottom-center",
+    });
+
+    router.replace("/");
+  };
 
   return (
     <div>
@@ -72,7 +92,7 @@ const UserReservationItem = ({ reservation }: UserReservationItemProps) => {
             </p>
           </div>
 
-          <Button variant="danger" className="mt-5">
+          <Button variant="danger" className="mt-5" onClick={handleDeleteClick}>
             Cancelar
           </Button>
         </div>
--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -3,9 +3,11 @@
 import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { Prisma, TripReservation } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 
 import UserReservationItem from "./components/UserReservationItem";
+import Link from "next/link";
+import Button from "@/components/Button";
 
 const MyTrips = () => {
   const [reservations, setReservations] = useState<
@@ -24,7 +26,9 @@ const MyTrips = () => {
     }
 
     const fetchReservations = async () => {
-      const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any).id}/reservations`);
+      const response = await fetch(
+        `http://localhost:3000/api/user/${(data?.user as any).id}/reservations`
+      );
 
       const json = await response.json();
 
@@ -36,10 +40,24 @@ const MyTrips = () => {
 
   return (
     <div className="container mx-auto p-5">
-      <h1 className="font-semibold text-primaryDarker text-xl">Minhas Viagens</h1>
-      {reservations?.map((reservation) => (
-        <UserReservationItem key={reservation.id} reservation={reservation} />
-      ))}
+      <h1 className="font-semibold text-primaryDarker text-xl">
+        Minhas Viagens
+      </h1>
+      {reservations.length > 0 ? (
+        reservations?.map((reservation) => (
+          <UserReservationItem key={reservation.id} reservation={reservation} />
+        ))
+      ) : (
+        <div className="flex flex-col">
+          <p className="mt-2 font-medium text-primaryDarker">
+            Você ainda não tem nenhuma reserva! =(
+          </p>
+
+          <Link href="/">
+            <Button className="w-full mt-2">Fazer reserva</Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
